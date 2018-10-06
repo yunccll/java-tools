27e4fe97b9221a9404b6c07f701803ea93fae762
yunccll <yunccll@sina.com>
2018-10-07 01:17:31 +0800

    add the log demo

--- basement/src/main/java/com/chenglun/App.java
+++ basement/src/main/java/com/chenglun/App.java
@@ -1,11 +1,15 @@
 package com.chenglun;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 /**
- * Hello world!
- *
  */
 public class App 
 {
+    final static Logger logger = LoggerFactory.getLogger(App.class);
+
     public void number(){
         {
             Integer i = 1245; 
@@ -52,14 +56,20 @@ public class App
         java.util.Date dt = new java.util.Date();
         System.out.printf("date:%1$tF %1$tT, %1$tr\n", dt);
     }
+    public void logger(){
+        logger.info("hello world");
+    }
 
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        org.apache.log4j.BasicConfigurator.configure();
+
         App app = new App();
         app.number();
         app.charF();
         app.stringBuilder();
         app.date();
+        app.logger();
     }
 }
